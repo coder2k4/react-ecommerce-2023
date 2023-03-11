972c31e9d4a519d976a389ad0777a32c6866c998
AlexeyGStandalone <coder2k@mail.ru>
2023-03-11 21:42:36 +0300

    sass global variables accessible in next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const path = require("path")
+
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    sassOptions: {
+        includePath: [path.join(__dirname, "styles")],
+        prependData: `@import "./base.scss";`
+    }
 }
 
 module.exports = nextConfig
